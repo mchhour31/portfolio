8e05ccc58517f8cd508f383b57905138851da042
mchhour31 <marlon1293@gmail.com>
2022-10-11 10:58:15 +1300

    added social icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 // import Container from './components/Container';
 import Header from './components/Header';
-import Introduction from './components/Introduction.jsx';
+import Introduction from './components/Introduction';
 
 
 function App() {
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { TwitterIcon, InstagramIcon, GitHubIcon, LinkedInIcon } from './SocialIcons';
 
@@ -14,10 +13,18 @@ const Introduction = () => {
                                     <h1 className="text-4xl font-bold tracking-tight text-white dark:text-zinc-100 sm:text-5xl">Hello world</h1>
                                     <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.</p>
                                     <div className="mt-6 flex gap-6">
-                                        <a href="/twitter.com">
-                                            <img src={TwitterIcon} alt="" />
+                                        <a href="https://twitter.com">
+                                            <TwitterIcon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
                                         </a>
-                                        {/* <TwitterIcon /> */}
+                                        <a href="https://twitter.com">
+                                            <InstagramIcon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+                                        </a>
+                                        <a href="https://github.com/mchhour31/">
+                                            <GitHubIcon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+                                        </a>
+                                        <a href="https://www.linkedin.com/in/mchhour/">
+                                            <LinkedInIcon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+                                        </a>
                                     </div>
                                 </div>
                             </div>
